c7ab3eb7e3184440bfac7f6123c2c1ca028140e0
Roger Winkler <rw@Rogers-MacBook-Pro.local>
2018-04-23 22:38:03 +0200

    db connection test using environment variables

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const { Pool, Client } = require('pg')
+
+
 const app = express() 		// define app to be an express app
 app.use(morgan('combined')) // use morgan log library to get logs while the server is running
 app.use(bodyParser.json())	// parse body of http request
@@ -12,6 +15,16 @@ app.post('/register', function(req, res) {
 	res.send({
 		message: 'Hello '+req.body.email+'! Your user was registered! Have Fun!'
 	})
+})
+
+app.get('/db', async function(req, res) {
+	const client = new Client()
+	await client.connect()
+
+	const result = await client.query('SELECT NOW()')
+	console.log(result.rows[0].now)
+	res.send(result.rows[0].now)
+	await client.end()
 })
 
 app.listen(process.env.PORT || 8081) 
